d04e1a63e8be8ee173356906a8e6cb098f4a48e4
Maurício de Carvalho <mauriciocr223@gmail.com>
2022-05-26 20:32:30 -0300

    feat: sending emails using nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "6f87ba4c60d8de",
+      pass: "2584b55e776fa6"
+    }
+  });
 
 app.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
             comment,
             screenshot
         }
+    });
+
+    await transport.sendMail({
+        from: "Equipe Feedget <exemplo@feedget.com>",
+        to: "Maurício de Carvalho <mauriciocr223@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}<p/>`,
+            `<p>Comentário: ${comment}<p/>`,
+            `<div/>`
+        ].join("\n")
     });
 
     return res.status(201).json({ data: feedback });
